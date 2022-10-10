aba2347be21f8b154a55d83b16527a878461254a
mahmud035 <mahamudulhasan4148@gmail.com>
2022-10-11 00:12:07 +0600

    setup react-router-dom and added errorPage

---
+++ src/Components/ErrorPage/ErrorPage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useRouteError } from 'react-router-dom';
+import './ErrorPage.css';
+
+const ErrorPage = () => {
+  const error = useRouteError();
+
+  return (
+    <div>
+      {error && (
+        <div className="error-page">
+          <h1>Oops!</h1>
+          <p>Sorry, an unexpected error has occurred.</p>
+          <p>
+            <i>{error.statusText || error.message}</i>
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ErrorPage;
